9a40ae5b717fcb295e9b192ad2ec4317ab991b61
Thy-Saonan <thysaonan007@gmail.com>
2023-09-14 20:10:37 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:saonan19/saonan19.github.io main
+git push -f git@github.com:saonan19/saonan19.github.io gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages

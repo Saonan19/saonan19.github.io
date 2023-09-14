43b7b9bf9032670969d269e895f1846f43b0a549
Thy-Saonan <thysaonan007@gmail.com>
2023-09-14 20:01:07 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,11 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:saonan19/saonan19.github.io.git main
+git push -f git@github.com:saonan19/saonan19.github.io main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
